80c839cb2d13e135ea1c93310476e8bf73bb191a
glauberdjs <112635086+glauberdjs@users.noreply.github.com>
2023-01-06 13:57:34 -0300

    Projeto funcional

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\CadastroController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PrincipalController;
-use App\Http\Controllers\TesteController;
-use Illuminate\Support\Facades\Route;
